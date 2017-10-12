7f6c83ec84f1f440f42caad1b67633b854595aa8
Tom <thomas.seaton@rungopher.com>
2017-10-12 13:24:37 +1100

    Add curl error handler

--- src/Requesters/CurlRequester.php
+++ src/Requesters/CurlRequester.php
@@ -16,17 +16,21 @@ class CurlRequester implements RequesterInterface {
 	public function newRequest(SimpleSmsRequest $request) {
 
 		curl_setopt_array($this->curl, array(
-    		CURLOPT_RETURNTRANSFER => 1,
-    		CURLOPT_URL => $request->getUrl(),
-    		CURLOPT_PORT => $request->getPort(),
-    		CURLOPT_USERPWD => "{$request->getUsername()}:{$request->getPassword()}",
-    		CURLOPT_POST => 1,
-    		CURLOPT_POSTFIELDS => $request->getBody()
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_URL => $request->getUrl(),
+			CURLOPT_PORT => $request->getPort(),
+			CURLOPT_USERPWD => "{$request->getUsername()}:{$request->getPassword()}",
+			CURLOPT_POST => 1,
+			CURLOPT_POSTFIELDS => $request->getBody()
 		));
 
-        $response = curl_exec($this->curl);
-        $statusCode = curl_getinfo($this->curl, CURLINFO_HTTP_CODE);
-        curl_close($this->curl);
+		$response = curl_exec($this->curl);
+		if (curl_errno($this->curl)) {
+			throw new SimpleSmsErrorException("Couldn't send request: " . curl_error($this->curl));
+		}
+		
+		$statusCode = curl_getinfo($this->curl, CURLINFO_HTTP_CODE);
+		curl_close($this->curl);
 
 		return new SimpleSmsResponse($statusCode, $response);       
 	}
